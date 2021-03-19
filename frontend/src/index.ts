655215ab3b00f4b4a9aa419b0a3095e61527b0a3
Hikaru Inomoto <medalhkr@gmail.com>
2021-03-19 14:35:36 +0000

    lit-element init

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,1 +1,16 @@
-console.log('aa');
+import {LitElement, html, customElement, property, css} from 'lit-element';
+
+@customElement('app-root')
+export class App extends LitElement {
+  @property()
+  msg = 'foo';
+
+  static styles = css`
+    :host {
+      color: red;
+    }
+  `;
+  render() {
+    return html`<p>${this.msg}</p>`;
+  }
+}
